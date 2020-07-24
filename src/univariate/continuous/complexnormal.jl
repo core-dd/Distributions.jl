4dd8470cf261dc8ba93bef94b6e54e95107795c7
Jens Abraham <jens.abraham@ntnu.no>
2020-07-24 14:50:39 +0200

    minor modifications

--- src/univariate/continuous/complexnormal.jl
+++ src/univariate/continuous/complexnormal.jl
@@ -8,10 +8,16 @@ f(x; \\mu, \\sigma) = \\frac{1}{πσ^2} \\exp \\left(-\\frac{1}{σ^2} (x-μ)^\\a
 ```
 
 It is related to the Normal distribution via
+TODO: doc
 
-Andersen et al. - 1995 - The Multivariate Complex Normal Distribution
+
+Andersen, H. H., M. Højbjerre, D. Sørensen, and P. S. Eriksen. “The Multivariate Complex Normal Distribution.” In Linear and Graphical Models, 15–37. Lecture Notes in Statistics. Springer, New York, NY, 1995. https://doi.org/10.1007/978-1-4612-4240-6_2.
+Picinbono, B. “Second-Order Complex Random Vectors and Normal Distributions.” IEEE Transactions on Signal Processing 44, no. 10 (October 1996): 2637–40. https://doi.org/10.1109/78.539051.
+
 
 """
+
+#NB: R has a multi variate implementation
 
 struct ComplexNormal{T<:Real} <: ContinuousUnivariateDistribution
   μ::Complex{T}
@@ -30,35 +36,36 @@ ComplexNormal(μ::Integer, σ::Integer) = ComplexNormal(float(μ) + 1.0im, float
 ComplexNormal(μ::Complex{Integer}, σ::Integer) = ComplexNormal(float(μ), float(σ))
 ComplexNormal(μ::T) where {T <: Real} = ComplexNormal(μ+1.0im, one(T))
 ComplexNormal(μ::T) where {T <: Complex{Real}} = ComplexNormal(μ, one(T))
-ComplexNormal() = ComplexNormal(0.0, 1.0, check_args=false)
+ComplexNormal() = ComplexNormal(0.0+0.0im, 1.0, check_args=false)
 
 # #### Conversions
-# TODO:
+# TODO: conversions
 
-@distr_support ComplexNormal -Inf-Inf*im Inf+Inf*im  # does that make sence in the grand scheme?
+# TODO: how to describe bounds of a complex random variable
+#@distr_support ComplexNormal -Inf-Inf*im Inf+Inf*im  # does that make sence in the grand scheme?
 
 #### Parameters
 
 params(d::ComplexNormal) = (d.μ, d.σ)
-@inline partype(d::ComplexNormal{T}) where {T<:Real} = T
+@inline partype(d::ComplexNormal{T}) where {T<:Real} = T # TODO: check
 
 location(d::ComplexNormal) = d.μ
 scale(d::ComplexNormal) = d.σ
 
-Base.eltype(::Type{ComplexNormal{T}}) where {T} = T
+Base.eltype(::Type{ComplexNormal{T}}) where {T} = Complex{T}
 
 #### Statistics
 
 mean(d::ComplexNormal) = d.μ
-#median(d::ComplexNormal) = d.μ # TODO: check
+median(d::ComplexNormal) = nothing # there is no unique way to order complex numbers
 mode(d::ComplexNormal) = d.μ
 
 var(d::ComplexNormal) = abs2(d.σ)
 std(d::ComplexNormal) = d.σ
-#skewness(d::ComplexNormal{T}) where {T<:Real} = zero(T) # TODO: check
-#kurtosis(d::ComplexNormal{T}) where {T<:Real} = zero(T) # TODO: check
+skewness(d::ComplexNormal{T}) where {T<:Real} = zero(T)
+kurtosis(d::ComplexNormal{T}) where {T<:Real} = zero(T)
 
-#entropy(d::ComplexNormal) = (log2π + 1)/2 + log(d.σ)  # TODO: check
+entropy(d::ComplexNormal) = log(π * d.σ) + 1
 
 #### Evaluation
 
